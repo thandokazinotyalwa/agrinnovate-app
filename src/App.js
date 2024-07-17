72942f29a3d88c7f1dd3e057f714852efc6de909
Mbalenhle36 <122805991+Mbalenhle36@users.noreply.github.com>
2024-07-17 21:55:27 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact Component=
-        {Home} />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/services" component={Services} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from './Button'; 
-import './Navbar.css';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -19,52 +18,56 @@ function Navbar() {
     }
   };
 
-  
   useEffect(() => {
     showButton();
-    window.addEventListener('resize', showButton);
+    window.addEventListener("resize", showButton);
 
-    return () =>window.addEventListener('resize', showButton);
-  },[]); 
-
-    
+    return () => window.addEventListener("resize", showButton);
+  }, []);
 
   return (
-    <nav className='navbar'>
-      <div className='navbar-container'>
-        <Link to='/' className='navbar-logo'>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
           AGRINNOVATE
         </Link>
-        <div className='menu-icon' onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fa-solid fa-bars'}></i>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fa-solid fa-bars"}></i>
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
               About
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='/opportunities' className='nav-links' onClick={closeMobileMenu}>
+          <li className="nav-item">
+            <Link
+              to="/opportunities"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
               Opportunities
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+          <li className="nav-item">
+            <Link
+              to="/sign-up"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
               Sign-up
             </Link>
           </li>
         </ul>
-        {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
       </div>
     </nav>
   );
 }
 
 export default Navbar;
-
