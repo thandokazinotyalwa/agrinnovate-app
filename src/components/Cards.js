d0750f0bd6d5f6e9440eed9da0d06dd73ca9fd89
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2024-07-19 12:23:07 +0200

    update navigation bar

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import "./Cards.css";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>What we offer</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
